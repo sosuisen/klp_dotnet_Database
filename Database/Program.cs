8aca74ecfcffaf3433a540dcbc328efc3d581d38
sosuisen <hidekazu.kubota@gmail.com>
2024-01-18 00:30:55 +0900

    adding..

--- Database/Program.cs
+++ Database/Program.cs
@@ -9,26 +9,29 @@ namespace Database {
                 context.Tasks.Add(new Task("Buy milk", DateTime.Now));
                 context.Tasks.Add(new Task("Buy PC", new DateTime(2023, 12, 24), true));
                 context.Tasks.Add(new Task("Buy chocolate", new DateTime(2024, 2, 14), true));
+                context.Tasks.Add(new Task("Clean the kitchen", DateTime.Now.AddDays(1), true));
                 context.SaveChanges();
 
                 // Update
-                var taskToUpdate = context.Tasks.FirstOrDefault(t => t.Name == "Buy milk");
+                var taskToUpdate = context.Tasks.FirstOrDefault(t => t.Name == "Buy PC");
                 if (taskToUpdate != null)
                 {
-                    taskToUpdate.Completed = true;
+                    taskToUpdate.Deadline = new DateTime(2024, 1, 1);
                     context.SaveChanges();
                 }
 
                 // Delete
-                var taskToDelete = context.Tasks.FirstOrDefault(t => t.Name == "Buy chocolate");
+                var taskToDelete = context.Tasks.Where(t => t.Deadline < new DateTime(2024, 1, 15)).Select(t => t); 
                 if (taskToDelete != null)
                 {
-                    context.Tasks.Remove(taskToDelete);
+                    foreach (var task in taskToDelete) {
+                        context.Tasks.Remove(task);
+                    }
                     context.SaveChanges();
                 }
 
                 // Read
-                var tasks = context.Tasks.Select(t => $"Name: {t.Name}, Deadline: {t.Deadline}, Completed: {t.Completed}");
+                var tasks = context.Tasks.Where(t => t.Completed == false).Select(t => $"Name: {t.Name}, Deadline: {t.Deadline}, Completed: {t.Completed}");
                 foreach (var item in tasks)
                 {
                     Console.WriteLine(item);
