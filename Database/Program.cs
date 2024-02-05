811a57a813babf9a3db3c5bfca6dfdad1b085c92
sosuisen <hidekazu.kubota@gmail.com>
2024-02-06 03:28:38 +0900

    fix: Task to ToDos

--- Database/Program.cs
+++ Database/Program.cs
@@ -12,22 +12,6 @@ namespace Database
                 context.ToDos.Add(new ToDo("Buy chocolate", new DateTime(2024, 2, 14), true));
                 context.SaveChanges();
 
-                // Update
-                var taskToUpdate = context.ToDos.FirstOrDefault(t => t.Name == "Buy milk");
-                if (taskToUpdate != null)
-                {
-                    taskToUpdate.Completed = true;
-                    context.SaveChanges();
-                }
-
-                // Delete
-                var taskToDelete = context.ToDos.FirstOrDefault(t => t.Name == "Buy chocolate");
-                if (taskToDelete != null)
-                {
-                    context.ToDos.Remove(taskToDelete);
-                    context.SaveChanges();
-                }
-
                 // Read
                 var date = new DateTime(2024, 1, 1);
                 var tasks2 = context.ToDos.Where(t => t.Deadline > date).Select(t => $"Name: {t.Name}, Deadline: {t.Deadline}, Completed: {t.Completed}");
